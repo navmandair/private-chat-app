8a9c7c11c167f0e6278f4daf7b843b95c214e77d
Navpreet Singh <mandair96@gmail.com>
2023-01-12 14:11:59 +1300

    done

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,85 @@
 const ws = io();
 
-ws.on('pull', (text)=>{
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = document.querySelector('#message-form').querySelector('#message')
+const $messageFormSubmitBtn = document.querySelector('#message-form').querySelector('#submit')
+const $shareLocationBtn = document.querySelector('#share-location')
+
+const $messagesDiv = document.querySelector('#messages')
+const $chatSidebar = document.querySelector('#sidebar')
+
+const $messageTemplate = document.querySelector('#message-template').innerHTML
+const $locationTemplate = document.querySelector('#location-template').innerHTML
+const $sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+const autoScroll = () => {
+    $messagesDiv.scrollTop = $messagesDiv.scrollHeight
+}
+
+ws.emit('join', {username, room}, (error) =>{
+    if(error){
+        alert(error)
+    } else {
+        console.log(`${username} joined ${room} room`)
+    }
+});
+
+ws.on('serverToClientTextMessage', (data)=>{
     //console.log('Message Received !', text);
-    document.getElementById('chatbox').value += text;
+    const html = Mustache.render($messageTemplate, {username: data.username, message: data.text, createdAt: moment(data.createdAt).format('h:mma')});
+    $messagesDiv.insertAdjacentHTML('beforeend', html)
+    autoScroll()
+})
+
+ws.on('serverToClientLocation', (data)=>{
+    //console.log('Location Received !', locationURL);
+    const html = Mustache.render($locationTemplate, {username: data.username, locationURL: data.url, createdAt: moment(data.createdAt).format('h:mma')});
+    $messagesDiv.insertAdjacentHTML('beforeend', html)
+    autoScroll()
+})
+
+ws.on('roomData', (data)=>{
+    //console.log(data.users);
+    const html = Mustache.render($sidebarTemplate, {room: data.room, users: data.users});
+    $chatSidebar.innerHTML = html
 })
 
 function sendMessage(e) {
     e.preventDefault(); 
-    const messagerBox = e.target.elements.message
-    const text = messagerBox.value
-    ws.emit('push', text)
+    $messageFormSubmitBtn.setAttribute('disabled', 'disabled');
+    const text = $messageFormInput.value
+    ws.emit('clientToServerTextMessage', text, (error)=>{
+        $messageFormSubmitBtn.removeAttribute('disabled');
+        if(error){
+            alert(error)
+        }
+        console.log('Message:', res)
+    })
     //console.log('Sent !', text)
-    messagerBox.value = ''
+    $messageFormInput.value = ''
 }
 
-document.querySelector('#message-form').addEventListener('submit', sendMessage);
+function shareLocation() {
+    if(!navigator.geolocation){
+        return alert('Location cannot be shared !')
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        $shareLocationBtn.setAttribute('disabled', 'disabled');
+        const locationData = {latitude: position.coords.latitude, longitude: position.coords.longitude}
+        ws.emit('clientToServerLocation', locationData, async (error)=>{
+            await new Promise(r => setTimeout(r, 1000));
+            $shareLocationBtn.removeAttribute('disabled');
+            if(error){
+                alert(error)
+            }
+            //console.log('Shared Location:', res)
+        })
+        //console.log(locationData)
+    })
+}
+
+$messageForm.addEventListener('submit', sendMessage);
+
+$shareLocationBtn.addEventListener('click', shareLocation);
--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app)
@@ -11,14 +14,70 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 io.on('connection', (ws) => {
     console.log('New WS Connection')
-    ws.emit('pull', 'Welcome to My Private Chat App')
-    ws.on('push', (text)=>{
-        io.emit('pull', '\n'+text)
+    ws.emit('serverToClientTextMessage', generateMessage('ChatBot', 'Welcome !'))
+    //ws.broadcast.emit('serverToClientTextMessage', generateMessage('New User Joined'))
+    ws.on('clientToServerTextMessage', (text, callback) => {
+        const { error, user } = getUser(ws.id)
+        if (error) {
+            return callback(error)
+        }
+        if (user) {
+            if (new Filter().isProfane(text)) {
+                callback('Profanity is not allowed')
+                return
+            }
+            io.to(user.room).emit('serverToClientTextMessage', generateMessage(user.username, text));
+            callback()
+        } else {
+            return callback('Session is invalid, Please log in again')
+        }
+    })
+
+    ws.on('clientToServerLocation', (locationData, callback) => {
+        const { error, user } = getUser(ws.id)
+        if (error) {
+            return callback(error)
+        }
+        if (user) {
+            io.emit('serverToClientLocation', generateLocationMessage(user.username, `https://google.com/maps?q=${locationData.latitude},${locationData.longitude}`))
+            callback()
+        } else {
+            return callback('Session is invalid, Please log in again')
+        }
+    })
+
+    ws.on('join', ({ username, room }, callback) => {
+        const { error, user } = addUser({ id: ws.id, username, room });
+        if (error) {
+            return callback(error)
+        } else {
+            ws.join(user.room)
+            ws.broadcast.to(user.room).emit('serverToClientTextMessage', generateMessage(`${user.username} has joined!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+            callback()
+        }
+    })
+
+    ws.on('disconnect', () => {
+        let { error, user } = removeUser(ws.id);
+        if (error) {
+            return
+        } else {
+            io.emit('serverToClientTextMessage', generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+
     })
 })
 
 const PORT = process.env.PORT || 3000;
 
-const init = (PORT) => { server.listen(PORT, () => { console.log('Express server started on', PORT)}) }
+const init = (PORT) => { server.listen(PORT, () => { console.log('Express server started on', PORT) }) }
 
 init(PORT);
